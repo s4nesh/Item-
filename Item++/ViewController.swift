c8ce51fdfab36cdac000bb7500bcc8431c08e7bb
Sanesh Gobin <saneshgobin88@gmail.com>
2023-05-18 18:11:18 +0200

    Created textBox, item add button and item lists in the form of UITextField. Created the variables, and button action to add item from textBox into first item list.

--- Item++/ViewController.swift
+++ Item++/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textBox: UITextField!
+    @IBOutlet weak var itemOneBox: UITextField!
+    @IBOutlet weak var itemBoxTwo: UITextField!
+    @IBOutlet weak var itemBoxThree: UITextField!
+    @IBOutlet weak var itemBoxFour: UITextField!
+    @IBOutlet weak var itemBoxFive: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func addButtonTapped(_ sender: Any) {
+        itemOneBox.text = textBox.text
+    }
+    
 }
 
