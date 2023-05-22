386e710bcb6cab579a3220df778cdad0198bfec6
Sanesh Gobin <saneshgobin88@gmail.com>
2023-05-22 18:18:47 +0200

    Added logic for button pressed action. Created an array with five empty string elements. They're a bug with logic: Only the first item is listed in the list.

--- Item++/ViewController.swift
+++ Item++/ViewController.swift
@@ -16,17 +16,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var itemBoxFour: UITextField!
     @IBOutlet weak var itemBoxFive: UITextField!
     
-    
+    var buttonTapCount = 0
+    var itemList: [String] = [" ", " ", " ", " "," "]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
     }
 
+    func addToItemArray() {
+        itemList[buttonTapCount] = textBox.text ?? " "
+    }
+    
+    func refreshList() {
+        itemOneBox.text = itemList[0]
+        itemBoxTwo.text = itemList[1]
+        itemBoxTwo.text = itemList[2]
+        itemBoxTwo.text = itemList[3]
+        itemBoxTwo.text = itemList[4]
+    }
+    
     @IBAction func addButtonTapped(_ sender: Any) {
-        itemOneBox.text = textBox.text
+    
+        if buttonTapCount > 4 {
+            print("You cannot add more items")
+            textBox.text = " "
+        } else {
+            addToItemArray()
+            textBox.text = " "
+            buttonTapCount += 1
+            
+        }
+        refreshList()
+        print(itemList)
     }
     
 }
